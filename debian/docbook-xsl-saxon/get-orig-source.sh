e59dad16cefbc6befae4eb6ec12cc37fae07532a
Siqueira <rodrigosiqueiramelo@gmail.com>
2017-12-19 19:54:59 -0200

    Added docbook-xsl-saxon

---
+++ debian/docbook-xsl-saxon/get-orig-source.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -ex
+
+UPSTREAM_VERSION=$2
+ORIG_TARBALL=$3
+
+REAL_TARBALL=`readlink -f ${ORIG_TARBALL}`
+
+WORKING_DIR=`dirname ${ORIG_TARBALL}`
+
+ORIG_TARBALL_DFSG=`echo ${ORIG_TARBALL} | sed -e "s/\(${UPSTREAM_VERSION}\)\(\.orig\)/\1+dfsg\2/g"`
+ORIG_TARBALL_DIR=`echo ${ORIG_TARBALL_DFSG} | sed -e "s/_\(${UPSTREAM_VERSION}\)/-\1/g" -e "s/\.tar\.gz//g"`
+ORIG_TARBALL_DIR_STRIP=`basename ${ORIG_TARBALL_DIR}`
+
+mkdir -p ${ORIG_TARBALL_DIR}
+tar --directory=${ORIG_TARBALL_DIR} --strip 1 -xzf ${REAL_TARBALL} || exit 1 
+rm -f ${ORIG_TARBALL} ${REAL_TARBALL}
+ant -f ${ORIG_TARBALL_DIR}/build.xml \
+    -Dplatform.active=debian \
+    -Dplatforms.debian.home=/usr/lib/jvm/default-java \
+    clean
+GZIP=-9 tar --remove-files --directory ${WORKING_DIR} -czf ${ORIG_TARBALL_DFSG} ${ORIG_TARBALL_DIR_STRIP} || exit 1
+
+exit 0
